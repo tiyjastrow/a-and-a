7f891a1f3c98f07bd3044ea1b2c3f4b10e92def5
Gil Pratte <gil@theironyard.com>
2017-08-05 17:07:54 -0500

    first steps to enabled web security

--- src/main/java/com/example/ana/controller/PersonController.java
+++ src/main/java/com/example/ana/controller/PersonController.java
@@ -44,4 +44,9 @@ public class PersonController {
         model.addAttribute("message", exception.getMessage());
         return "error_message";
     }
+
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
 }
